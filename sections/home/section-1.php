add9c68135696b07b0637a8a11cab8a63e08195b
Marcus <imkay2712@gmail.com>
2023-02-15 15:51:56 +0700

    done: sec-1 desktop

--- sections/home/section-1.php
+++ sections/home/section-1.php
@@ -8,9 +8,9 @@
                 <div class="right_content_sec_1">
                     <?php echo get_field('content_right_sec_1'); ?>
                 </div>
-                <div class="mc-line"></div>
+                <div class="mc-line"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/line-1.png" class="search-black" alt="Search Black"></div>
                 <div class="btn_sec_1">
-                    <a href="<?php echo get_field('link_button_sec_1'); ?>"><?php echo get_field('text_button_sec_1'); ?></a>
+                    <a href="<?php echo get_field('link_button_sec_1'); ?>"><span><i class="fa-brands fa-youtube"></i></span><?php echo get_field('text_button_sec_1'); ?></a>
                 </div>
             </div>
         </div>
