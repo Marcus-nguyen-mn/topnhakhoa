3d3d41e6db6a6abd22b8a3533940499872bf25be
Marcus <imkay2712@gmail.com>
2023-02-15 15:21:14 +0700

    build sec 1

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -5,7 +5,7 @@
 get_header();
 ?>
 <?php
-    // get_template_part('sections/home/focus'); 
+    get_template_part('sections/home/section-1'); 
 ?>
 
 <?php get_footer();?>
---
+++ sections/home/section-1.php
@@ -0,0 +1,18 @@
+<section class="section_1">
+    <div class="section_1_contain">
+        <div class="sec-1-row">
+            <div class="left-banner">
+                <img src="<?php echo get_field('sec_1_banner_left'); ?>" alt="<?php echo get_field('sec_1_banner_left'); ?>">
+            </div>
+            <div class="right-contain">
+                <div class="right_content_sec_1">
+                    <?php echo get_field('content_right_sec_1'); ?>
+                </div>
+                <div class="mc-line"></div>
+                <div class="btn_sec_1">
+                    <a href="<?php echo get_field('link_button_sec_1'); ?>"><?php echo get_field('text_button_sec_1'); ?></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
